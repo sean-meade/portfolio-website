ef8e11e21f18e5785d2078f740e63d5a2a97f360
Sean Meade <seanpatmeade@gmail.com>
2024-03-01 16:23:15 +0000

    fix tag in image of card search

--- static/js/script.js
+++ static/js/script.js
@@ -2,3 +2,17 @@ function SearchTag(checkbox) {
 
     document.searchProjects.submit();
 }
+
+function SearchTagCard(tag) {
+
+
+    document.querySelectorAll('input[type=checkbox]').forEach(el =>{
+        el.checked = false;
+        if (el.value == tag.value) {
+            el.checked = true;
+        };
+    });
+
+
+    document.searchProjects.submit();
+}
