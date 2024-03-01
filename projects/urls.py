5459cb870a4a9ea4c808a9a96a723cef9744cc41
Sean Meade <seanpatmeade@gmail.com>
2024-03-01 14:59:42 +0000

    remove landing page

--- projects/urls.py
+++ projects/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from projects import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.projects, name='home'),
     path('projects/', views.projects, name='projects'),
     path('projects_tag/<int:tag_id>', views.projects_tag, name='projects_tag'),
 ]
